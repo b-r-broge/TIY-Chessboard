bfb9b8f74f5b475b0b5da5687adf48cb846bac5c
Brad Broge <brbroge@gmail.com>
2017-06-11 08:28:43 -0400

    making progress towards adding pieces

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Create a website that will demonstrate and play through the first moves of
+// the catalan opening for chess.
+
+// var chessBoard =
+//     [['wr', 'wk', 'wb', 'wq', 'wk', 'wb', 'wk', 'wr'],
+//      ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['.', '.', '.', '.', '.', '.', '.', '.'],
+//      ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+//      ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br']];
+// let wp = './images/wp.png', wr = './images/wr.png', wn = './images/wn.png';
+// let wk = './images/wk.png', wq = './images/wq.png', wb = './images/wb.png';
+// let bp = './images/bp.png', br = './images/br.png', bn = './images/bn.png';
+// let bb = './images/bb.png', bk = './images/bk.png', bq = './images/bq.png';
 
 function genBoard() {
   // Create 64 divs, separated into classes of light and dark, and rows and columns
@@ -19,20 +37,21 @@ function genBoard() {
 
   //loop to create the actual board itself
   for (let i = 1 ; i <= 8 ; i++) {
-    for (let j = 65 ; j <= 72 ; j++) {
+    for (let j = 72 ; j >= 65 ; j--) {
       let tile = document.createElement('div');
       let loc = String.fromCharCode(j).toLowerCase() + i;
       if ((i % 2 === 0 && j % 2 === 1) || (i % 2 === 1 && j % 2 === 0 )) {
-        tile.className = "tile dark " + loc;
+        tile.className = "tile dark"
+        tile.id = loc;
       } else {
-        tile.className = "tile light " + loc;
+        tile.className = "tile light"
+        tile.id = loc;
       }
       // console.log(loc);
       tile.style.gridArea = loc;
       board.appendChild(tile);
     }
   }
-
 }
 
 function genSidebar(isRow) {
@@ -46,7 +65,7 @@ function genSidebar(isRow) {
   if (isRow) {
     var vals = ["1", "2", "3", "4", "5", "6", "7", "8"];
   } else {
-    var vals = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var vals = ["h", "g", "f", "e", "d", "c", "b", "a"];
   }
 
   for (let i = 0; i < vals.length ; i++) {
@@ -54,16 +73,43 @@ function genSidebar(isRow) {
     newTile.textContent = vals[i];
     newSidebar.appendChild(newTile);
   }
-
   return newSidebar;
 }
 
-function populatePieces() {
+function placePieces() {
   // I need to pull the piece img files and place them in the board grid.
-  // I'll create a div within the tile div and scale the background to fit.
-  // Going to try and have the black pieces be inverted in color
-  // from the white pieces
-  
+  // I'll create a div within the tile div and scale the background to contain.
+  let startingPieces = [['wr', 'a1', 'h1'], ['wn', 'b1', 'g1'], ['wb', 'c1', 'f1'],
+        ['wk', 'e1'], ['wq', 'd1'], ['wp', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+        ['br', 'a8', 'h8'], ['bn', 'b8', 'g8'], ['bb', 'c8', 'f8'], ['bk', 'e8'],
+        ['bq', 'd8'], ['bp', 'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7']];
+  for (let i = 0 ; i < startingPieces.length ; i++) {
+    for (let j = 1 ; j < startingPieces[i].length ; j++) {
+      // let loc = String.fromCharCode(j).toLowerCase() + i;
+      movePieces("", startingPieces[i][j], startingPieces[i][0]);
+    }
+  }
+}
+
+function movePieces(start, finish, piece) {
+  // this function will find the piece on the start tile, and move it
+  // to the finish tile.
+  // If start is "" then just place a piece on the finish tile.
+  if (start !== "") {
+    // remove piece from class on start tile
+    let startTile = document.getElementById(start);
+    let classes = startTile.className;
+    let pos = classes.indexOf(piece);
+    if (pos >= 0 ) {
+      classes = classes.slice(pos-1);
+    }
+    startTile.className = classes;
+  }
+  // Add piece class to finish tile
+  let endTile = document.getElementById(finish);
+  endTile.className += " " + piece;
+
 }
 
 genBoard();
+placePieces();
